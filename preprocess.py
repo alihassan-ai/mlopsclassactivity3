9ac2b2096c59907017eccaeba0a4a46be1cfa160
Ali Hassan <alihassanai2026@gmail.com>
2025-11-12 23:56:47 +0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,12 @@
+import os 
 import pandas as pd
 from sklearn.datasets import load_iris
 
 iris = load_iris()
 data = pd.DataFrame(iris.data, columns=iris.feature_names)
 data['target'] = iris.target
+
+os.makedirs("data", exist_ok=True)
 
 data.to_csv("data/preprocessed.csv", index=False)
 print("✅ Data preprocessed and saved to data/preprocessed.csv")
